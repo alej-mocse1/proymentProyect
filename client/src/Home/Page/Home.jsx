44d0f8e1edf9a2141f2eecd56da146b76cd4ed8e
tobiastymkiw <tobiastymkiw@gmail.com>
2023-03-13 17:15:59 -0300

    feat(src/Home/Page/Cards) cree la primera parte del home

---
+++ client/src/Home/Page/Home.jsx
@@ -0,0 +1,24 @@
+import Card from "./Card/Card";
+import styles from "./Home.module.css";
+import { info } from "./assets/info";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.container1}>
+        <h4 className={styles.h4}>ADN Proyment</h4>
+        <p className={styles.p}>
+          Alcanzamos de forma correcta la transformación empresarial en base a
+          la
+          <br></br>
+          innovación y el potenciamiento a escala
+        </p>
+      </div>
+      <div className={styles.cards}>
+        {info.length
+          ? info.map((info) => <Card key={info.id} info={info} />)
+          : "loading"}
+      </div>
+    </div>
+  );
+}

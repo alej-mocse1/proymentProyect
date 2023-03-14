dd5da2d74bea297b00751495536170317e5f4311
tobiastymkiw <tobiastymkiw@gmail.com>
2023-03-13 22:19:16 -0300

    Fix(src/Home) movi las funciones para que quede ordenado el home

---
+++ client/src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/NavBar/NavBar";
+import Page from "./Page/Page";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Page></Page>
+    </>
+  );
+}

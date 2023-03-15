e40c36f61ce409b13c36d29fcae32c58a1d00996
alej-mocse1 <alejandromcse01@gmail.com>
2023-03-15 10:47:03 -0300

    botones flotantes

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../Components/NavBar/NavBar";
 import LandinPage from './ladinPage1/landinPage1'
 import Page from "./Page/Page";
+import LandinPage2 from "./landinpage2/landinPage2";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar></Navbar>
       <LandinPage></LandinPage>
       <Page></Page>
+      <LandinPage2></LandinPage2>
     </>
   );
 }

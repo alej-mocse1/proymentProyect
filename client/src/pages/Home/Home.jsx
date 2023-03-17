75658fcd0d4a00e8a75fc9f9f2c038326cd1d747
tobiastymkiw <tobiastymkiw@gmail.com>
2023-03-17 03:27:46 -0300

    react-router-dom general button navigation component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,16 +4,18 @@ import LandinPage from '../../features/Home.feature/ladinPage1/landinPage1'
 import Page from "../../features/Home.feature/Page";
 import ServiciosAtuMedida from "../../features/Home.feature/ServiciosAtuMedida/landinPage2";
 import PorqueProyment from "../../features/Home.feature/PorqueProyment/PorqueProyment";
+import Button from "../../core/components/Button/Button";
 
 export default function Home() {
   return (
     <>
       {/* <Navbar></Navbar>
-      <LandinPage></LandinPage> */}
+      <LandinPage></LandinPage>
       <Page></Page>
-      {/* <ServiciosAtuMedida></ServiciosAtuMedida>
-      <PorqueProyment></PorqueProyment> */}
-      <Footer></Footer>
+      <ServiciosAtuMedida></ServiciosAtuMedida>
+      <PorqueProyment></PorqueProyment>
+      <Footer></Footer> */}
+      <Button></Button>
     </>
   );
 }

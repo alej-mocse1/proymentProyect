bcea9df685264e8f53bd993500ecafca271ffca8
JosemaCeballos <josema.soyhenry@gmail.com>
2023-03-17 11:40:58 -0300

    fix: rama de Ale traída

--- client/src/features/Home.feature/LideresEnLatinoamerica/LideresEnLatinoamerica.jsx
+++ client/src/features/Home.feature/LideresEnLatinoamerica/LideresEnLatinoamerica.jsx
@@ -1,10 +1,71 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./LideresEnLatinoamerica.module.css";
+
 import img1 from "./assets/AvatarCircle01.png";
 import img2 from "./assets/AvatarCircle02.png";
 import img3 from "./assets/AvatarCircle03.png";
 
+import LideresHIJO from "./LideresHijo";
+
 const LideresEnLatinoamerica = () => {
+  const [state, setState] = useState([
+    {
+      img: img1,
+      name: "Leandro rocha",
+      titulo: "Project Manager en SP",
+      frase:
+        " Proyment fue ágil desde el primer contacto, valoro la simplicidad y la rapides para solucionar problemas especificos",
+    },
+    {
+      img: img2,
+      name: "Jonh Montoro",
+      titulo: "Gerente de Operaciones en Tom Gutie Company ",
+      frase:
+        " Nuestra experiencia con Proyment fue excelente, pudieron brindranos un conjunto de planes orientado a nuestras necesidades. luego de dos meses de arduo trabajo, pudimos cumplir con los objetivos empresariales . ",
+    },
+    {
+      img: img3,
+      name: "Matt Cannon",
+      titulo: "Marketing director at ABN8 Trading ",
+      frase:
+        " Tivemos que realizar un importante lançamento de marca no Brasil e a Proyment contribuiu em grande parte para os ótimos que tivemos ",
+    },
+  ]);
+  const [item, seTitems] = useState(state[0]);
+  const [correnPage, setCurrentPage] = useState(0);
+
+  const nextPage = () => {
+    const FirstIndex = correnPage + 1;
+
+    //pregunto si FirstIndex es mayor que el total de videojuego corto con el return
+    if (FirstIndex > 2) {
+      seTitems(state[0]);
+      setCurrentPage(0);
+      return;
+    }
+
+    //cambio mi estado local item(que es el que se renderiza)
+    seTitems(state[FirstIndex]);
+
+    //cambio mi estado loca correnPage
+    setCurrentPage(FirstIndex);
+  };
+
+  const prevPage = () => {
+    const prevPage = correnPage - 1;
+
+    if (prevPage < 0) {
+      seTitems(state[2]);
+      setCurrentPage(2);
+      return;
+    }
+
+    seTitems(state[prevPage]);
+
+    setCurrentPage(prevPage);
+  };
+
   return (
     <div className={styles.div}>
       <h2>Líderes en latinoamerica nos recomiendan</h2>
@@ -35,7 +96,7 @@ const LideresEnLatinoamerica = () => {
           </h2>
 
           <div className={styles.infoPersona}>
-            <img src={img2} alt="img1" />
+            <img src={img2} alt="img2" />
 
             <div className={styles.divTEXTiNFO}>
               <h3>Jonh Montoro</h3>
@@ -61,25 +122,14 @@ const LideresEnLatinoamerica = () => {
         </div>
       </div>
 
-      <div className={styles.DivRESPONSIVE}>
-        <div className={styles.divResponsiveFrase}>
-          <p>
-            Nuestra experiencia con Proyment fue excelente, pudieron brindranos
-            un conjunto de planes orientado a nuestras necesidades. luego de dos
-            meses de arduo trabajo, pudimos cumplir con los objetivos
-            empresariales .
-          </p>
-        </div>
-
-        <div className={styles.DivInfoResponsive}>
-          <img src={img1} alt="img1" />
-
-          <div className={styles.infoContactoResponsive}>
-            <h3>Matt Cannon</h3>
-            <p>Marketing director at ABN8 Trading </p>
-          </div>
-        </div>
-      </div>
+      <LideresHIJO
+        frase={item.frase}
+        name={item.name}
+        img={item.img}
+        titulo={item.titulo}
+        nextPage={nextPage}
+        prevPage={prevPage}
+      ></LideresHIJO>
     </div>
   );
 };
---
+++ client/src/features/Home.feature/LideresEnLatinoamerica/LideresHijo.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./LideresEnLatinoamerica.module.css";
+
+const LideresHIJO = (props) => {
+  return (
+    <div className={styles.DivRESPONSIVE}>
+      <div className={styles.divResponsiveFrase}>
+        <p>{props.frase}</p>
+      </div>
+
+      <div className={styles.DivInfoResponsive}>
+        <img src={props.img} alt="img1" />
+
+        <div className={styles.infoContactoResponsive}>
+          <h3>{props.name}</h3>
+          <p>{props.titulo}</p>
+        </div>
+      </div>
+
+      <div className={styles.DivBTNS}>
+        <button className={styles.btnIZQ} onClick={() => props.prevPage()}>
+          {"<"}
+        </button>
+        <button className={styles.btnDER} onClick={() => props.nextPage()}>
+          {">"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LideresHIJO;

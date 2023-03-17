f011cab96c7460416b774926387960e0e72ab798
JosemaCeballos <josema.soyhenry@gmail.com>
2023-03-17 12:01:27 -0300

    fix: code in order

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,10 @@
 import Home from "./pages/Home/Home";
-import Navbar from "./Components/NavBar/NavBar";
-import LandinPage from "./features/Home.feature/ladinPage1/landinPage1";
-import ADN from "./features/Home.feature/ADNProyment/ADN";
-import LandinPage2 from "./features/Home.feature/landinpage2/landinPage2";
-import OPT from "./features/Home.feature/OPTProyment/OPTProyment";
-import Services from "./features/Home.feature/Services/Services";
-import PorqueProyment from "./features/Home.feature/PorqueProyment/PorqueProyment";
-import Transformation from "./features/Home.feature/Transformation/Transformation";
-import CompaniesFriendly from "./features/Home.feature/CompaniesFriendlY/CompaniesFriendly.jsx";
-import LideresEnLationamerica from "./features/Home.feature/LideresEnLatinoamerica/LideresEnLatinoamerica"
-import Footer from "./Components/Footer/Footer";
-import DesafíosDeTodoNivel from "./features/Home.feature/DesafíosDeTodoNivel/DesafíosDeTodoNivel";
+
 //comment
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <LandinPage />
-      <ADN />
-      <OPT />
-      <Services />
-      <LandinPage2 />
-      <PorqueProyment />
-      <Transformation />
-      <CompaniesFriendly />
-      <LideresEnLationamerica/>
-      <DesafíosDeTodoNivel/>
-      <Footer/>
+      <Home/>
     </div>
   );
 }
--- client/src/features/Home.feature/Page.jsx
+++ client/src/features/Home.feature/Page.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import Services from "./Services/Services";
 import ADN from "./ADNProyment/ADN";
 import OPT from "./OPTProyment/OPTProyment";
+import LandinPage from "./ladinPage1/landinPage1";
+import LandinPage2 from "./landinpage2/landinPage2";
+import PorqueProyment from "./PorqueProyment/PorqueProyment";
+import Transformation from "./Transformation/Transformation";
+import CompaniesFriendly from "./CompaniesFriendlY/CompaniesFriendly";
+import LideresEnLatinoamerica from "./LideresEnLatinoamerica/LideresEnLatinoamerica";
+import DesafíosDeTodoNivel from "./DesafíosDeTodoNivel/DesafíosDeTodoNivel";
 
 export default function Page() {
   return (
     <>
+      <LandinPage />
       <ADN></ADN>
       <OPT></OPT>
       <Services></Services>
+      <LandinPage2 />
+      <PorqueProyment />
+      <Transformation />
+      <CompaniesFriendly />
+      <LideresEnLatinoamerica />
+      <DesafíosDeTodoNivel />
     </>
   );
 }

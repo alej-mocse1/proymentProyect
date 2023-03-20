2ca6225d9bfaddd8a1cc0e87a413fefd2718c215
FranzDuran <franzduran.fd@gmail.com>
2023-03-20 11:21:41 -0300

    carrusel

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/NavBar/NavBar";
 import Page from "../../features/Home.feature/Page";
+import Carrusel from "../../features/Home.feature/Transformation/Carrusel";
 
 export default function Home() {
   return (
     <>
-      <Navbar></Navbar>
+      {/* <Navbar></Navbar> */}
       <Page></Page>
+      <Carrusel/>
       <Footer></Footer>
     </>
   );

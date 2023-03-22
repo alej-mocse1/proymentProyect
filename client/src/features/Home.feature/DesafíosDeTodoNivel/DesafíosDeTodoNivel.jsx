9bb8094c01b2e75349aa80482ccab362df0e3d12
José Maria Ceballos <josema.soyhenry@gmail.com>
2023-03-22 13:08:45 -0300

    Update DesafíosDeTodoNivel.jsx

--- client/src/features/Home.feature/DesafíosDeTodoNivel/DesafíosDeTodoNivel.jsx
+++ client/src/features/Home.feature/DesafíosDeTodoNivel/DesafíosDeTodoNivel.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./DesafiosDeTodoNivel.scss";
 import background from "./imgCierre.png";
+import Button from "../../../core/components/Button/Button";
+import grafico from "./imgFlotant.png";
 
 function DesafíosDeTodoNivel() {
   return (
     <div className="div2">
+      <div className="grafico2">
+        <img src={grafico} alt="grafico2" className="imgGrafico2" />
+      </div>
+
       <div className="div--row2">
         <h1>Nos adaptamos a desafíos de todo nivel</h1>
         <span>
@@ -12,9 +18,8 @@ function DesafíosDeTodoNivel() {
           tecnólogica, siendo un impulsor para optimizar las companías con
           visión global.
         </span>
-        <div className="button-container2">
-          <button className="button-container--reunion2">Agendar reunión</button>
-        </div>
+
+        <Button className="button-container2" />
       </div>
 
       <div className="div--row2 container2">

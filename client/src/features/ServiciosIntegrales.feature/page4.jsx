8e6cbdd51791a2a7c2dd26f6009fb542b604d242
tobiastymkiw <tobiastymkiw@gmail.com>
2023-03-29 17:29:37 -0300

    Fix styles en Planes y componente de Integrales

--- client/src/features/ServiciosIntegrales.feature/page4.jsx
+++ client/src/features/ServiciosIntegrales.feature/page4.jsx
@@ -1,9 +1,11 @@
 import LandingPageServIntegrales from "./LandingPageServIntegrales/LandingPageServIntegrales";
+import NuestrosPlanes from './NuestrosPlanes/NuestrosPlanes'
 
 export default function Page4() {
   return (
     <>
       <LandingPageServIntegrales></LandingPageServIntegrales>
+      <NuestrosPlanes></NuestrosPlanes>
     </>
   );
 }

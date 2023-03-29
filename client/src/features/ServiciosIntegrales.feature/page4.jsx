6df75ad95992b3ec05e2d521912ad41d703dc81c
alej-mocse1 <alejandromcse01@gmail.com>
2023-03-29 16:58:19 -0300

    logos de serv.integrales(destkop y mobile)

--- client/src/features/ServiciosIntegrales.feature/page4.jsx
+++ client/src/features/ServiciosIntegrales.feature/page4.jsx
@@ -1,9 +1,11 @@
 import LandingPageServIntegrales from "./LandingPageServIntegrales/LandingPageServIntegrales";
+import SistemasConLosQueAyudamos from "./Sistemas/SistemasConLosQueAyudamos";
 
 export default function Page4() {
   return (
     <>
       <LandingPageServIntegrales></LandingPageServIntegrales>
+      <SistemasConLosQueAyudamos></SistemasConLosQueAyudamos>  
     </>
   );
 }

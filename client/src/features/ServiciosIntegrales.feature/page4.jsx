6eee60bd1c42ef0d37418a472512b86c328868f3
tobiastymkiw <tobiastymkiw@gmail.com>
2023-03-30 16:00:15 -0300

    Servicios integrales (Desktop)

--- client/src/features/ServiciosIntegrales.feature/page4.jsx
+++ client/src/features/ServiciosIntegrales.feature/page4.jsx
@@ -1,11 +1,13 @@
 import LandingPageServIntegrales from "./LandingPageServIntegrales/LandingPageServIntegrales";
 import NuestrosPlanes from './NuestrosPlanes/NuestrosPlanes'
+import ServIntPerf from "./ServicioIntegralPerfecto/ServIntPerf";
 
 export default function Page4() {
   return (
     <>
       <LandingPageServIntegrales></LandingPageServIntegrales>
-      <NuestrosPlanes></NuestrosPlanes>
+      <NuestrosPlanes></NuestrosPlanes>{/* 4 */}
+      <ServIntPerf></ServIntPerf>{/* 6 */}
     </>
   );
 }

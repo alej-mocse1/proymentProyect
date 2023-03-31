8dacc65e176ed97409b6605b05d564382900679a
alej-mocse1 <alejandromcse01@gmail.com>
2023-03-31 02:36:07 -0300

    terminado el componente Cada área tiene un indicador de productividad.·;

--- client/src/features/ServiciosAnalisisDeDatos.feature/Page5.jsx
+++ client/src/features/ServiciosAnalisisDeDatos.feature/Page5.jsx
@@ -1,5 +1,6 @@
 import CadaVezMas from "./CadaVezMas/CadaVezMas";
 import LandingPage from "./LandingPage/LandingPage";
+import Logos from "./Logos/logos";
 import NosDiferencia from "./NosDiferencia/NosDiferencia";
 import NuestrasSoluciones from "./NuestrasSoluciones/NuestrasSoluciones";
 import TomaDeDecisiones from "./TomaDeDecisiones/TomaDeDecisiones";
@@ -9,6 +10,7 @@ export default function Page5(){
     return(
         <div>
             <LandingPage/>
+            {/* <Logos></Logos> */}
             <TomaDeDecisiones/>
             <NosDiferencia/>
             <NuestrasSoluciones/>

aff6ef36c28dd2f5636237df36e9b73b352e2397
alej-mocse1 <alejandromcse01@gmail.com>
2023-03-31 12:48:52 -0300

    commit para traer main

--- client/src/features/ServiciosAnalisisDeDatos.feature/Page5.jsx
+++ client/src/features/ServiciosAnalisisDeDatos.feature/Page5.jsx
@@ -1,6 +1,6 @@
 import CadaVezMas from "./CadaVezMas/CadaVezMas";
 import LandingPage from "./LandingPage/LandingPage";
-/* import Logos from "./Logos/logos"; */
+import Logos from "./Logos/logos";
 import NosDiferencia from "./NosDiferencia/NosDiferencia";
 import NuestrasSoluciones from "./NuestrasSoluciones/NuestrasSoluciones";
 import NuestrasSolucionesAreas from "./NuestrasSolucionesAreas/NuestrasSolucionesAreas";

01c9d6b38112631b5be7a1f7150a948f25d1738f
alej-mocse1 <alejandromcse01@gmail.com>
2023-03-31 20:11:31 -0300

    commmit para traer main

---
+++ client/src/features/ServiciosEnTendencia.feature/LasEmpresasConfianEnNosotros/LasEmpresasConfianEnNosotros.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./LasEmpresasConfianEnNosotros.module.scss"
+
+
+const LasEmpresasConfianEnNosotros = () => {
+
+    return(
+        <div className={styles.div}>
+
+        </div>
+    )
+}
+
+
+export default LasEmpresasConfianEnNosotros

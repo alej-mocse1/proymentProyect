d668aba2622d4f44b033a3629dac4ca24bb3c356
JosemaCeballos <josema.soyhenry@gmail.com>
2023-03-31 14:44:58 -0300

    test

--- client/src/pages/servicios/servicios.jsx
+++ client/src/pages/servicios/servicios.jsx
@@ -1,7 +1,7 @@
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/NavBar/NavBar";
 import Page2 from "../../features/Servicios.feature/page2";
-
+// que paso
 
 export default function Servicios() {
   return (

7d7da582f2abb1086efd747a6870664315bfcdff
tobiastymkiw <tobiastymkiw@gmail.com>
2023-04-03 18:23:28 -0300

    NuestrosPlanes UXUI Hecho

--- client/src/features/ServiciosUXUI/Page7.jsx
+++ client/src/features/ServiciosUXUI/Page7.jsx
@@ -1,12 +1,14 @@
 import LandingPageServiciosUXUI from "./LandingPageServiciosUXUI/LandingPageServiciosUXUI";
+import NuestrosPlanes from "./NuestrosPlanes/NuestrosPlanes";
 import ServiciosQuePrestamos from "./ServiciosQuePrestamos/ServiciosQuePrestamos";
 
-
-export default function Page7(){
-    return(
-        <>
-        <LandingPageServiciosUXUI/>
-        <ServiciosQuePrestamos/>
-        </>
-    );
+export default function Page7() {
+  return (
+    <>
+      <LandingPageServiciosUXUI />
+      <ServiciosQuePrestamos />
+      {/* Componnente de Jose */}
+      <NuestrosPlanes />
+    </>
+  );
 }

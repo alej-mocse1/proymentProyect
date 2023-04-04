b02bf4ea92d7fcec2825e005e30df91c0f6df566
FranzDuran <franzduran.fd@gmail.com>
2023-04-04 02:14:14 -0300

    etapas duracion

--- client/src/features/ServiciosUXUI/Page7.jsx
+++ client/src/features/ServiciosUXUI/Page7.jsx
@@ -1,10 +1,11 @@
 import ServiciosQuePrestamos from "./ServiciosQuePrestamos/ServiciosQuePrestamos";
-
+import EtapasDuracion from "./EtapasDuracion/EtapasDuracion";
 
 export default function Page7(){
     return(
         <>
         <ServiciosQuePrestamos/>
+        <EtapasDuracion/>
         </>
     );
 }

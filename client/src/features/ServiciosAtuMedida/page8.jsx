c49c9e0aecc040fec942d04ddbecfc3961638aa4
alej-mocse1 <alejandromcse01@gmail.com>
2023-04-04 17:09:36 -0300

    cada vez mas empresas confian.. terminado, faltan correcciones

--- client/src/features/ServiciosAtuMedida/page8.jsx
+++ client/src/features/ServiciosAtuMedida/page8.jsx
@@ -4,6 +4,7 @@ import LandingServAtuMedida from "./LadinSerAMedida/LandingServAtuMedida";
 import NuestrosPlanes from "./NuestrosPlanes/NuestrosPlanes";
 import Icons from "./Icons/Icons";
 import NuestrosServicios from "./NuestrosServicios/NuestrosServicios";
+import CadaVezMasEmpresasConfian from "./CadaVezMasEmpresasConfian/CadaVezMasEmpresasConfian";
 
 
 const Page8 = () => {
@@ -16,6 +17,7 @@ const Page8 = () => {
             <NuestrosPlanes></NuestrosPlanes>
             <NuestrosServicios/>
             <Contribuimos/>
+            <CadaVezMasEmpresasConfian></CadaVezMasEmpresasConfian>
         </div>
     )
 
